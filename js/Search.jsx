6fdbadbaec7bf3ff5bc7cf4c9b727a4b1ac9cbf6
shyenuganti <shyenuganti@gmail.com>
2018-06-17 12:52:47 -0400

    Linking both pages with links
    Used the 'Link' component to link both the pages to each other for easy navigation.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const Search = () => <h1> Your Route works ..This is the search page.</h1>
+const Search = () => (
+ <div>   
+    <h1 style={{ color:"blue" }}> Your Route works ..This is the search page.</h1>
+    <Link to="/" style={{ color:"red" }}> Go to Homepage</Link>
+</div>
+);
 
 export default Search;
