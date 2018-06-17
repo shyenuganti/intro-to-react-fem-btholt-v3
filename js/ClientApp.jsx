a4996ac5799507a116acf4c6ec95dd76d8cfa83b
shyenuganti <shyenuganti@gmail.com>
2018-06-17 12:44:41 -0400

    Search route added
    Added a second route to the app. Just a basic barebones page to verify routing.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
+import Search from './Search';
 
 const App = () => (
   <HashRouter>
     <div className="app">
-    <Route exact path="/" component={Landing} />    
+    <Route exact path="/" component={Landing} />
+    <Route exact path="/search" component={Search} />    
   </div>
   </HashRouter>
 
 );
-   
+
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ js/Search.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search = () => <h1> Your Route works ..This is the search page.</h1>
+
+export default Search;
