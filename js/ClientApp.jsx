83867457a58c2908eb76ae5fbb82c565477d65b0
shyenuganti <shyenuganti@gmail.com>
2018-06-17 14:16:50 -0400

    ErrorPage for unknown routes
    historyApiFallback: true - Gives control to the browser if the server couldnot find the orute instead of returning 404. This essentially returns index page .
    
    Browser routing has to configure the error page. If no error or fallback page configured, the index page is served to the customer.
    
    ErrorPage configured in this commit. Note that we started new component Switch from the router module to make this work.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+
+const ErrorPage = () => <h1 style={{ color:"red" }}>Oops the route cannot be found</h1>
 
 const App = () => (
   <BrowserRouter>
     <div className="app">
+    <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/search" component={Search} />    
+        <Route path="/search" component={Search} />
+        <Route component={ErrorPage} />      
+    </Switch>
     </div>
   </BrowserRouter>
-
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
