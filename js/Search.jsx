8c474f96212596a82c3bbf23f22082f9ecbf662f
shyenuganti <shyenuganti@gmail.com>
2018-06-17 15:33:34 -0400

    Adding ShowCard Module
    Extracting the show details into its own module and include it in the search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 const Search = () => (
  <div className="search">   
-    { preload.shows.map( (show) => (
-        <div className='show-card'>
-            <img alt={ `${show.title} Show Poster` } src={ `/public/img/posters/${show.poster}`} />       
-            <div>
-                <h3>{show.title}</h3>
-                <h4>({show.year})</h4>
-                <p>{show.description}</p>            
-            </div>
-        
-        </div>
-     )) }
+    { preload.shows.map(show => <ShowCard show={show} />) }
 </div>
 );
 
