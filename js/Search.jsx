44301d2c4754edbf2e040a5f3ea2367a7b480718
shyenuganti <shyenuganti@gmail.com>
2018-06-17 23:49:27 -0400

    Search is now Stateful :)
    Converting Search component from a stateless function component to stateful react component.
    Must:
    All Stateful components extends 'Component'
    Has to implement render() method
    render should always return markup.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
+import { Component } from 'react';
 
-const Search = () => (
- <div className="search"> 
-    <header>
-        <h1>svideo</h1>
-        <input type="text" placeholder="Search"/>
-    </header>
-    <div>
-    { preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />) }
-    </div>
-</div>
-);
+class Search extends Component {
+ render() {   
+   return (  
+        <div className="search"> 
+            <header>
+                <h1>svideo</h1>
+                <input type="text" placeholder="Search"/>
+            </header>
+            <div>
+            { preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />) }
+            </div>
+        </div>
+    );
+  }
+}
 
 export default Search;
