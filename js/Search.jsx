9a73e45640f77cafaf2a6ff24c76bf39b16871ea
shyenuganti <shyenuganti@gmail.com>
2018-06-17 23:55:17 -0400

    Adding state to the component
    Once you extend Component, you get access to a ton of react methods and variables. state is one of them.
    
    We inittialize the state in the constructor and use it anywhere in the markup.
    
    No external component can ever the state of the Search component. Strictly prohibited.

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,12 +4,20 @@ import preload from '../data.json';
 import { Component } from 'react';
 
 class Search extends Component {
- render() {   
+
+  constructor (props) {
+    super (props);
+    this.state = {
+        searchTerm: 'Want to find something?'
+    };
+  }
+
+  render() {   
    return (  
         <div className="search"> 
             <header>
                 <h1>svideo</h1>
-                <input type="text" placeholder="Search"/>
+                <input value={this.state.searchTerm} type="text" placeholder="Search"/>
             </header>
             <div>
             { preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />) }
