92a2da3c2663b5f2327a7e8ba0f57bb28705f96f
shyenuganti <shyenuganti@gmail.com>
2018-06-21 20:42:19 -0400

    Revert "First Test - Its just JEST !"
    This reverts commit 725ec984c83e05cae98ee7e052198ad04750cbe3.

--- js/__tests__/Search.spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Search from '../Search';
-
-test('Search renders correctly', () => {
-    const component =  renderer.create(<Search />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
