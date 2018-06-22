e5464ec84dd02727929d126b090981dfca188e69
shyenuganti <shyenuganti@gmail.com>
2018-06-21 20:15:54 -0400

    Adding FLow to our project
    We are adding Flow to our project. And download all the flow typings from their repository using flow-typed plugin.

--- js/Search.jsx
+++ js/Search.jsx
@@ -11,12 +11,16 @@ class Search extends Component {
         searchTerm: ''
     };
 
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    //this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
   }
 
-  handleSearchTermChange (event) {
-      this.setState({ searchTerm: event.target.value});
-  }
+//   handleSearchTermChange (event) {
+//       this.setState({ searchTerm: event.target.value});
+//   }
+
+    handleSearchTermChange = event => {
+        this.setState( { searchTerm: event.target.value} );
+    }
 
   render() {   
    return (  
