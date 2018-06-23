1076c852c8917e96faddede1d8a174c04635dc8f
shyenuganti <shyenuganti@gmail.com>
2018-06-23 12:10:50 -0400

    Converting Details from a functional Component to Class Component
    In order to use the react lifecycle methods, Details component should class type and not function type. We are making changes to make it a react class.

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
-
-const Details = (props) => {
-    console.log(props);
-    const {title, description, year, poster, trailer} = props.show;
-    return (
-        <div className="details">
-           <Header />
-            <section>
-                <h1>{title}</h1>
-                <h2>({year})</h2>
-                <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
-                <p>{description}</p>
-            </section>
-            <div>
-                <iframe
-                  src={`https://www.youtube-nocookie.com/embed/${trailer}?
-                  rel=0&amp;
-                  controls=0&amp;
-                  showinfo=0`}
-                  frameBorder="0"
-                  allowFullScreen
-                  title={`Trailer for ${title}`}
-                />
+class Details extends Component {
+    
+    render(){
+        return (
+            <div className="details">
+            <Header />
+                <section>
+                    <h1>{this.props.show.title}</h1>
+                    <h2>({this.props.show.year})</h2>
+                    <img src={`/public/img/posters/${this.props.show.poster}`} alt={`Poster for ${this.props.show.title}`} />
+                    <p>{this.props.description}</p>
+                </section>
+                <div>
+                    <iframe
+                    src={`https://www.youtube-nocookie.com/embed/${this.props.show.trailer}?
+                    rel=0&amp;
+                    controls=0&amp;
+                    showinfo=0`}
+                    frameBorder="0"
+                    allowFullScreen
+                    title={`Trailer for ${this.props.show.title}`}
+                    />
+                </div>
             </div>
-        </div>
-    );
+        )
+    };
 }
 
 export default Details;     
