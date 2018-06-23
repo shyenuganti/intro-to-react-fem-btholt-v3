251f27fe243c25b557d68bf74ad4311bb77177e6
shyenuganti <shyenuganti@gmail.com>
2018-06-23 13:04:52 -0400

    Get rating by AJAX  call
    Adding a constructor to set default state.
    
    Adding the componentDidMount life cycle method to fetch rating data from our local api server that we started in our earlier step.
    
    We make a call and add the data to our state.
    
    We check if apiData is present in the state to show rating. If yes show rating or else show spinner.
    
    I added console.log for debugging.

--- js/Details.jsx
+++ js/Details.jsx
@@ -4,16 +4,40 @@ import Spinner from './Spinner';
 import axios from 'axios';
 
 class Details extends Component {
-    
+
+    constructor (props) {
+        super (props);
+        this.state = {
+            rating: ''
+        };
+    }
+        
+    componentDidMount(){
+        axios.get(`http://localhost:3000/${this.props.show.imdbID}`)
+        .then( (response) => {
+            console.log(response);
+            this.setState( { apiData: response.data } );
+        });
+    }
+
     render(){
+
+        let ratingComponent;
+        if(this.state.apiData){
+            ratingComponent = <h3>{this.state.apiData.rating}</h3>
+        } else {
+            ratingComponent = <Spinner />
+        }
+
         return (
             <div className="details">
             <Header />
                 <section>
                     <h1>{this.props.show.title}</h1>
                     <h2>({this.props.show.year})</h2>
+                    {ratingComponent}
                     <img src={`/public/img/posters/${this.props.show.poster}`} alt={`Poster for ${this.props.show.title}`} />
-                    <p>{this.props.description}</p>
+                    <p>{this.props.show.description}</p>
                 </section>
                 <div>
                     <iframe
