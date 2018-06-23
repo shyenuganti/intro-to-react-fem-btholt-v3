8df61a584c0f49845e8a1e0c45034d2c8071b695
shyenuganti <shyenuganti@gmail.com>
2018-06-23 11:38:00 -0400

    Conditionals in React
    Using conditional logic to display a back button on Details page

--- js/Header.jsx
+++ js/Header.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 
-const Header = () => {
+const Header = (props) => {
+    let utilSpace;
+    if(props.showSearch){
+        utilspace=<h1>Nothing Here..lol</h1>;
+    }else{
+        utilSpace= (<h2><Link to='/search'>Back</Link></h2>);
+    }
     return (
         <header>
             <h1><Link to='/'>svideo</Link></h1>
+            {utilSpace}
         </header>
     );
 }
+
+Header.defaultProps = {
+    showSearch: false
+};
 
 export default Header;
