ff715513e40aee4fd587d5a3f402377eccb6a7f5
shyenuganti <shyenuganti@gmail.com>
2018-06-23 14:11:09 -0400

    Setting up REDUX
    Adding default state, default action and the root reducer

---
+++ js/reducers.js
@@ -0,0 +1,14 @@
+const DEFAULT_STATE = {
+    searchTerm = ''
+};
+
+const rootReducer = (state = DEFAULT_STATE, action) => {
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
